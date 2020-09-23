e6182c82888e9e621a3987f5e6f167a5b7781217
chipo <pnapoli33@gmail.com>
2020-09-23 16:47:13 -0300

    Show discounts with binding.

--- src/app/budget/form.component.ts
+++ src/app/budget/form.component.ts
@@ -4,6 +4,7 @@ import { Company } from '../company/company';
 import { Payment } from '../payment/payment';
 import { Discount } from '../discount/discount';
 import { PaymentCondition } from '../payment-condition/payment-condition';
+import { BudgetDiscountLine } from '../budget-discount-line/budget-discount-line';
 
 @Component({
   selector: 'app-form',
@@ -16,17 +17,44 @@ export class FormComponent implements OnInit {
   public discount: Discount = new Discount();
   public titulo: string = 'Nuevo presupuesto';
   public paymentCondition: PaymentCondition = new PaymentCondition();
+  public budgetDiscountLine: BudgetDiscountLine = new BudgetDiscountLine();
 
   constructor() {}
 
   ngOnInit(): void {
     this.budget.company = this.company;
     this.budget.payment = this.payment;
-    this.budget.discount = this.discount;
     this.budget.paymentCondition = this.paymentCondition;
+    this.budget.budgetDiscountLines = new Array()
+    this.budgetDiscountLine.discount = new Discount();
   }
 
+  // actionsHandler(actionType) {
+  //   if (actionType === "add") {
+  //     console.log('>>> add');
+  //   } else if(actionType === "create") {
+  //     console.log('>>> create');
+  //   }
+  // }
+
   public create(): void {
-    console.log('###', this.budget);
+    // console.log('>>>', this.budget);
+    console.log('>>> CLICKED');
   }
+
+  /*
+
+  addDiscount(): void {
+    // console.log('>>[form.component] ', this.budgetDiscountLine.discount.id)
+    // this.budget.budgetDiscountLines.push(this.budgetDiscountLine);
+
+    // console.log('>>[form.component] length: ', this.budget.budgetDiscountLines.length);
+    // console.log('>>[form.component] array: ', this.budget.budgetDiscountLines);
+
+    // this.budgetDiscountLine = new BudgetDiscountLine();
+    // this.budgetDiscountLine.discount = new Discount();
+
+    console.log('addDiscount');
+  }
+  */
 }
--- src/app/discount/discount.component.ts
+++ src/app/discount/discount.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Discount } from './discount';
 import { DiscountService } from './discount.service';
-import { Budget } from '../budget/budget';
+import { BudgetDiscountLine } from '../budget-discount-line/budget-discount-line';
 
 @Component({
-  selector: 'select-discount[budget]',
+  selector: 'select-discount[budgetDiscountLine]',
   templateUrl: './discount.component.html',
 })
 export class DiscountComponent implements OnInit {
@@ -12,7 +12,7 @@ export class DiscountComponent implements OnInit {
   discounts: Discount[];
   public selectedDiscount: Discount = { id: 0, value: null, description: ''};
 
-  @Input() budget: Budget;
+  @Input() budgetDiscountLine: BudgetDiscountLine;
 
   constructor(
     private discountService: DiscountService
