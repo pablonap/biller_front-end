cd4e2ca0dcbb7479aed14f464af0e377580a9a1b
chipo <pnapoli33@gmail.com>
2020-09-22 23:05:58 -0300

    Add functionallity with Company select

--- src/app/budget/form.component.ts
+++ src/app/budget/form.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Budget } from './budget';
+import { Company } from '../company/company';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
 })
 export class FormComponent implements OnInit {
+  public budget: Budget = new Budget();
+  public company: Company = new Company();
+  public titulo: string = 'Nuevo presupuesto';
+  public paymentCondition: number[] = [30, 60, 120];
 
-  public budget: Budget = new Budget()
-  public titulo:string = "Nuevo presupuesto"
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.budget.clientName = 'quito';
+    this.budget.company = this.company;
   }
 
   public create(): void {
-    console.log("Clicked!")
-    console.log(this.budget)
+    console.log('Clicked!');
+    console.log('###', this.budget);
   }
-
 }
--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Company } from './company';
 import { CompanyService } from './company.service';
+import {Budget} from '../budget/budget';
 
 @Component({
-  selector: 'select-company',
+  selector: 'select-company[budget]',
   templateUrl: './company.component.html',
 })
 export class CompanyComponent implements OnInit {
@@ -11,11 +12,14 @@ export class CompanyComponent implements OnInit {
   companies: Company[];
   public selectedCompany: Company = { id: 0, name: '', address: '', email: '', phone: '' };
 
+  @Input() budget: Budget;
+
   constructor(
     private companyService: CompanyService
   ) {}
 
   ngOnInit(): void {
+    // console.log('>>> [company]', this.budget)
     this.companyService.getCompanies().subscribe((companies) => (this.companies = companies));
   }
 }
