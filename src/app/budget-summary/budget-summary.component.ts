ea602ba3d61f737575e744b0b2ec7e4ca865ffa7
chipo <pnapoli33@gmail.com>
2020-09-24 19:41:57 -0300

    Generate PDF

--- src/app/budget-summary/budget-summary.component.ts
+++ src/app/budget-summary/budget-summary.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef ,ViewChild} from '@angular/core';  
+// import * as jspdf from 'jspdf';  
+import {jsPDF} from 'jspdf';
+import html2canvas from 'html2canvas';  
+
 import { Budget } from '../budget/budget';
 import { BudgetSummaryService } from './budget-summary.service';
 import {Router, ActivatedRoute} from '@angular/router'
@@ -8,6 +12,24 @@ import {Router, ActivatedRoute} from '@angular/router'
   templateUrl: './budget-summary.component.html',
 })
 export class BudgetSummaryComponent implements OnInit {
+
+    public captureScreen()  
+  {  
+    var data = document.getElementById('contentToConvert');  
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      var imgWidth = 208;   
+      var pageHeight = 295;    
+      var imgHeight = canvas.height * imgWidth / canvas.width;  
+      var heightLeft = imgHeight;  
+  
+      const contentDataURL = canvas.toDataURL('image/png')  
+      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save('MYPdf.pdf'); // Generated PDF   
+    });  
+  }  
 
   public budget: Budget = new Budget();
 
